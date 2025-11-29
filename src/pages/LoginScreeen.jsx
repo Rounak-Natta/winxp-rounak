757a1091a7a7d5ef8eeef421b3c6aa54d609f0a0
Rounak-Natta <dev.rounaknatta@gmail.com>
2025-11-29 17:51:22 +0530

    Windows XP Portfolio Updated folder Structure

--- src/pages/LoginScreeen.jsx
+++ src/pages/LoginScreeen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Xplogo from "../assets/xplogo.png";
 import Avatar from "../assets/avatar.jpeg";
+import Shutdown from "../assets/Shutdown.png";
 
 const LoginScreeen = () => {
   return (
@@ -10,20 +11,33 @@ const LoginScreeen = () => {
       <div className="login-card">
         {/* LEFT SIDE */}
         <div className="login-card-left">
-          <img src={Xplogo} alt="XP Logo" className="xp-logo" />
-          <p className="login-text">To begin, click your user name</p>
+          <div className="div-content-logo">
+            <img src={Xplogo} alt="XP Logo" className="xp-logo" />
+          </div>
+          <p className="login-text">To Begin, Click Your User Name</p>
         </div>
-
         {/* RIGHT SIDE */}
         <div className="login-card-right">
           <div className="user-box">
             <img src={Avatar} alt="User Avatar" className="avatar" />
-            <h5 className="username">Rounak Natta</h5>
+            <div className="username-text-box">
+              <h5 className="username">Rounak Natta</h5>
+              <p className="username-text">4 Programs are running</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="login-down" />
+     <div className="login-down">
+      <div className="bottom-tab-left">
+        <img src={Shutdown} alt="" />
+        <h4>Turn Off Computer</h4>
+      </div>
+      <div className="bottom-tab-right">
+        <p>After you log on, you can add or change account <br />
+        Just Go to Control Panel and Click User accounts</p>
+      </div>
+     </div>
     </div>
   );
 };
